b42defb87c63bf24bd17283c3017df12eee98e33
Akshay Soni <akshaysoni460@gmail.com>
2021-03-16 11:09:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,4 +40,6 @@ def predict():
         return render_template('Front_end.html')
 
 if __name__=="__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+#     app.run(debug=True)
