b28c5aab0a15c9ba6ef160dd731f69bef4a95f08
Akshay.Soni <akshay.soni@OPLPT086.local>
2024-07-13 21:58:47 +0530

    FastApi Integration along with streamlit front end

---
+++ backend/app/router/router.py
@@ -0,0 +1,1 @@
+"""Routes for Server"""from api.api import Appprint("Router")# Create an instance of APp with a specific prefixrouter_instance = App(prefix="/v1")# Add routes to the class-based routerrouter_instance.router.add_api_route(    "/app/predict", App.predict, methods=["POST"], response_model=dict)router_instance.router.add_api_route("/app", App.home, methods=["GET"])
